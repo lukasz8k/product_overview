3e53bce76c5d4c0de58a6b5dd0aa3fcccaf49085
Łukasz Kaźmierski <>
2026-01-19 00:26:55 +0100

    update: main

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,23 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'core/theme/app_theme.dart';
+import 'router/app_router.dart';
 
 void main() {
-  runApp(const MainApp());
+  runApp(const ProviderScope(child: ProductOverviewApp()));
 }
 
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
+/// Main application widget
+class ProductOverviewApp extends StatelessWidget {
+  const ProductOverviewApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
-      ),
+    return MaterialApp.router(
+      title: 'Product Overview',
+      debugShowCheckedModeBanner: false,
+      theme: AppTheme.darkTheme,
+      routerConfig: appRouter,
     );
   }
 }
