ec8ce2f8eb66ebabf7dc40bc8ebc86f37a57b26e
Łukasz Kaźmierski <>
2026-01-18 19:42:07 +0100

    feat: data models

---
+++ lib/data/models/product.dart
@@ -0,0 +1,29 @@
+import 'package:json_annotation/json_annotation.dart';
+import 'rating.dart';
+
+part 'product.g.dart';
+
+@JsonSerializable()
+class Product {
+  final int id;
+  final String title;
+  final double price;
+  final String description;
+  final String category;
+  final String image;
+  final Rating rating;
+
+  const Product({
+    required this.id,
+    required this.title,
+    required this.price,
+    required this.description,
+    required this.category,
+    required this.image,
+    required this.rating,
+  });
+
+  factory Product.fromJson(Map<String, dynamic> json) => _$ProductFromJson(json);
+
+  Map<String, dynamic> toJson() => _$ProductToJson(this);
+}
---
+++ lib/data/models/rating.dart
@@ -0,0 +1,15 @@
+import 'package:json_annotation/json_annotation.dart';
+
+part 'rating.g.dart';
+
+@JsonSerializable()
+class Rating {
+  final double rate;
+  final int count;
+
+  const Rating({required this.rate, required this.count});
+
+  factory Rating.fromJson(Map<String, dynamic> json) => _$RatingFromJson(json);
+
+  Map<String, dynamic> toJson() => _$RatingToJson(this);
+}
